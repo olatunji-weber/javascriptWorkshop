59b4acf94bb98481374babeb772ac13f6e69c686
Olatunji Olayinka <olatunji.weber@gmail.com>
2022-03-08 05:01:19 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-document.querySelector("li a").style.color = "red";
-document.querySelector("h1").classList.add("huge");
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var choosenImage1 = "./images/dice" + randomNumber1 + ".png";
+//var choosenImage2 = ".dice" + randomNumber1 + "png";
+// img1.src = choosenImage1;
+//img2.src = choosenImage2;
+document.querySelector(".img1").src = choosenImage1;
